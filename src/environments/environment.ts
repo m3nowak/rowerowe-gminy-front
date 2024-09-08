236f717199159e16435eb51701fe26abb5ebaee5
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-09-08 17:43:07 +0200

    fix broken link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { NgxLoggerLevel } from "ngx-logger";
 export const environment = {
     baseBorderUrl: 'https://static.rowerowegminy.pl/geojsonfixedlite/',
     admInfoUrl: 'https://static.rowerowegminy.pl/adm/combolc.json.gz',
-    borderInfoUrl: "https://static.rowerowegminy.pl/gs200.json",
+    borderInfoUrl: "https://static.rowerowegminy.pl/gs200.json.gz",
     coaBaseUrl: 'https://static.rowerowegminy.pl/coa/',
     loggerLevel: NgxLoggerLevel.WARN
 };
