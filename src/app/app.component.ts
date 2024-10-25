fd53ff03c75a96c513f4426a454c9164d313869b
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-10-26 00:08:14 +0200

    No more material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
@@ -13,7 +9,7 @@ import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, FormsModule, MainLayoutComponent],
+  imports: [RouterOutlet, FormsModule, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
