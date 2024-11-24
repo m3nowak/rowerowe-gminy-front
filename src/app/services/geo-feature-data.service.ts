da7563176d2029878c6fd99f6285cd6b12ca2650
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-11-24 23:05:37 +0100

    Change geo feature data

--- src/app/services/geo-feature-data.service.ts
+++ src/app/services/geo-feature-data.service.ts
@@ -12,18 +12,63 @@ export class GeoFeatureDataService {
     partialConfig: { context: 'GeoFeatureDataService' },
   });
 
-  unlockedFeatures = new Set(['02', '3013', '3004', '3011', '3005032', '0812013', '0662011', '3022', '1606', '3029012']);
+  unlockedFeatures = new Set([
+    'PL0206031',
+    'PL0206041',
+    'PL0206052',
+    'PL0206072',
+    'PL0207042',
+    'PL0219021',
+    'PL0219043',
+    'PL0219083',
+    'PL0221072',
+    'PL0223043',
+    'PL0223062',
+    'PL0261011',
+    'PL0264011',
+    'PL1007032',
+    'PL1007062',
+    'PL1010012',
+    'PL1010072',
+    'PL1010093',
+    'PL1012102',
+    'PL1012113',
+    'PL1012132',
+    'PL1012142',
+    'PL1016011',
+    'PL1016053',
+    'PL1016092',
+    'PL1401013',
+    'PL1401022',
+    'PL1401052',
+    'PL1401063',
+    'PL1406073',
+    'PL1406083',
+    'PL1406113',
+    'PL1407042',
+    'PL1407063',
+    'PL1423043',
+    'PL2404063',
+    'PL2404092',
+    'PL2416073',
+    'PL2416083',
+    'PL2416102',
+    'PL2602072',
+    'PL2613012',
+    'PL2613052',
+  ]);
 
   borderInfoExt = computed<FeatureCollection<Geometry, GeoJsonProperties> | undefined>(() => {
+    const featuresSimplified = new Set(Array.from(this.unlockedFeatures).map((id) => id.replace('PL', '')));
     const allBorders = this.bordersSvc.borderInfo();
     if (allBorders) {
       const features_ext = allBorders.features.map((f) => {
         let unlockedArea = 'NONE';
-        if (this.unlockedFeatures.has(f.properties!['VOI_ID'])) {
+        if (featuresSimplified.has(f.properties!['VOI_ID'])) {
           unlockedArea = 'WOJ';
-        } else if (this.unlockedFeatures.has(f.properties!['COU_ID'])) {
+        } else if (featuresSimplified.has(f.properties!['COU_ID'])) {
           unlockedArea = 'POW';
-        } else if (this.unlockedFeatures.has(f.properties!['TERYT'])) {
+        } else if (featuresSimplified.has(f.properties!['TERYT'])) {
           unlockedArea = 'GMI';
         }
         return {
