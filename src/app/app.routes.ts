8451de087f3679d3e40bbf1cf18111cbe60c5704
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2025-02-18 21:25:11 +0100

    Remove unused route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { inject } from '@angular/core';
 import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
 import { RemoveQueryParamsGuard } from './guards/remove-query-params.guard';
 import { LoginPurgatoryComponent } from './components/login-purgatory/login-purgatory.component';
@@ -10,14 +8,6 @@ export const routes: Routes = [
     path: 'authorized',
     component: LoginPurgatoryComponent,
   },
-  // {
-  //   path: 'authorized',
-  //   redirectTo: ({ queryParams }) => {
-  //     const extAuthSvc = inject(AuthService);
-  //     extAuthSvc.feedToken(queryParams);
-  //     return 'home';
-  //   },
-  // },
   { path: 'home', loadComponent: () => import('./components/map-ui/map-ui.component').then((m) => m.MapUiComponent), canActivate: [RemoveQueryParamsGuard] },
   { path: '', component: WelcomeScreenComponent },
 ];
