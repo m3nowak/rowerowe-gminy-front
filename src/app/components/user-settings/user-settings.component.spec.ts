448acaeb280f96d6f8feb9df3336f4f41b009450
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2025-04-20 22:42:44 +0200

    Fix formatting in user settings component test setup

--- src/app/components/user-settings/user-settings.component.spec.ts
+++ src/app/components/user-settings/user-settings.component.spec.ts
@@ -8,9 +8,8 @@ describe('UserSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserSettingsComponent]
-    })
-    .compileComponents();
+      imports: [UserSettingsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserSettingsComponent);
     component = fixture.componentInstance;
